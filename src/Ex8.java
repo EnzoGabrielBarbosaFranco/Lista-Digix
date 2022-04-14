bef9b2262332fbf6aedd93e970a20e844b2c41ad
EnzoGabrielBarbosaFranco <enzoddos7@gmail.com>
2022-04-14 16:19:07 -0400

    Envio das atividades

--- src/Ex8.java
+++ src/Ex8.java
@@ -1,5 +1,7 @@
 public class Ex8 {
     public static void main(String[] args) {
-        
+        int decimal = 45;
+        String binario = Integer.toBinaryString(decimal);
+        System.out.println("O valor binario de " + decimal + "é" + binario);
     }
 }
