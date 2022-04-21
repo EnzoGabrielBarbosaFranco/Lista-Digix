f25d22eb0594d219359ac1b8a6378ed5cfbf76dd
EnzoGabrielBarbosaFranco <enzoddos7@gmail.com>
2022-04-21 13:40:20 -0400

    Atividade

---
+++ src/Ex9.java
@@ -0,0 +1,7 @@
+public class Ex9 {
+    public static void main(String[] args) {
+        int i = 8;
+        String binstr = Integer.toBinaryString(i);
+        System.out.println(binstr);
+    }
+}
