38cde142d81f0dd84b18527ac1297d13bed54deb
EnzoGabrielBarbosaFranco <enzoddos7@gmail.com>
2022-04-21 15:40:52 -0400

    Atividade

---
+++ src/Ex19.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex19 {
+    private static String str;
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String x = "25";
+        int y;
+
+        y = Integer.parseInt(x);
+        System.err.println("Sua String convertida em número é: " + y);
+    }
+}
