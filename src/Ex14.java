fa29998f7a4eb8f78539db42232ad2ca591b0376
EnzoGabrielBarbosaFranco <enzoddos7@gmail.com>
2022-04-21 15:16:33 -0400

    Atividade

---
+++ src/Ex14.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ex14 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Insira sua senha: ");
+        String senha = leitor.nextLine();
+        System.out.println("Sua senha atual é: " + senha);
+    }
+}
